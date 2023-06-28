852022ed4b8617057187d126605dbf142bda71b9
YzzilT <e-jt@live.nl>
2023-06-28 13:33:57 +0200

    first commit

---
+++ src/test/java/com/example/backendeindopdracht/BackendEindopdrachtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendeindopdracht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEindopdrachtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
