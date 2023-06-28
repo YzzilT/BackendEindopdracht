7b503c3545d535957dd242b25e55d2724d25571e
YzzilT <e-jt@live.nl>
2023-06-28 15:18:32 +0200

    <Feat> added product DTO'S

---
+++ src/main/java/com/example/backendeindopdracht/DTO/outputDto/ProductOutputDto.java
@@ -0,0 +1,13 @@
+package com.example.backendeindopdracht.DTO.outputDto;
+
+
+
+public class ProductOutputDto {
+
+    private Long id;
+    private String name;
+    private double price;
+    private int originalStock;
+    private int currentStock;
+    private String description;
+}
