fa347aa2c422ad5c7133bd04a3e8828074e6e103
YzzilT <e-jt@live.nl>
2023-06-30 17:33:43 +0200

    <Feat> create repair repository

---
+++ src/main/java/com/example/backendeindopdracht/repository/RepairRepository.java
@@ -0,0 +1,7 @@
+package com.example.backendeindopdracht.repository;
+
+import com.example.backendeindopdracht.model.Repair;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RepairRepository extends CrudRepository<Repair, Long> {
+}
