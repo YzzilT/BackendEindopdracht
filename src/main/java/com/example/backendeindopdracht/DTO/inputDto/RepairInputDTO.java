1a7c8e611f618e2a393b69f83b4b1790ac5b779c
YzzilT <e-jt@live.nl>
2023-06-30 17:38:48 +0200

    <Feat> create repair dto's

---
+++ src/main/java/com/example/backendeindopdracht/DTO/inputDto/RepairInputDTO.java
@@ -0,0 +1,18 @@
+package com.example.backendeindopdracht.DTO.inputDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RepairInputDTO {
+
+    private Long repairNumber;
+    private String customerName;
+    private String productName;
+    private String problemDescription;
+}
---
+++ src/main/java/com/example/backendeindopdracht/DTO/outputDto/RepairOutputDTO.java
@@ -0,0 +1,18 @@
+package com.example.backendeindopdracht.DTO.outputDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RepairOutputDTO {
+
+    private Long repairNumber;
+    private String customerName;
+    private String productName;
+    private String problemDescription;
+}
